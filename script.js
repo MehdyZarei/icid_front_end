11f9489c1064fbc7eb671e57956eb6d1a835565a
Mehdi Zarei <mehdis@Mehdis-MacBook-Air.local>
2024-07-13 14:32:36 -0400

    FronEnd/Dev_002 - dropdown menu; first connection to lambda function_01

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('dropdownButton').addEventListener('click', function ()
     var dropdownContent = document.getElementById('dropdownContent');
     dropdownContent.innerHTML = '';
 
-    fetch('https://vprefzsq6kuof7kult6iz6emea0baeau.lambda-url.us-east-2.on.aws/')
+    fetch('https://y7hqsirgg2.execute-api.us-east-2.amazonaws.com/dev')
     .then(response => console.log(response)).catch(error => {
         console.error('Error Fetching data:', error);
     });
