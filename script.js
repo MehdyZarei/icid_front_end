34291ec127f936774a5446a98cc5c22b834f1a27
Mehdi Zarei <mehdis@Mehdis-MacBook-Air.local>
2024-07-13 14:16:13 -0400

    FronEnd/Dev_002 - dropdown menu; first connection to lambda function

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-var callAPI = (agency, version)=>{
-    // instantiate a headers object
-    var myHeaders = new Headers();
-    // add content type header to object
-    myHeaders.append("Content-Type", "application/json");
-    // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"agency": agency, "version": version});
-    // create a JSON object with parameters for API call and store in a variable
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-    // make API call with parameters and use promises to get response
-    fetch("https://y7hqsirgg2.execute-api.us-east-2.amazonaws.com/dev", requestOptions)
-    .then(response => response.text())
-    .then(result => alert(JSON.parse(result).body))
-    .catch(error => console.log('error', error));
+document.getElementById('dropdownButton').addEventListener('click', function ()
+{
+    var dropdownContent = document.getElementById('dropdownContent');
+    dropdownContent.innerHTML = '';
+
+    fetch('https://vprefzsq6kuof7kult6iz6emea0baeau.lambda-url.us-east-2.on.aws/')
+    .then(response => console.log(response)).catch(error => {
+        console.error('Error Fetching data:', error);
+    });
+    
 }
+);
