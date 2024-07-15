e58dfb88d5633e9574ab19dc5394b39d6e583f84
Mehdi Zarei <mehdis@Mehdis-MacBook-Air.local>
2024-07-14 21:36:12 -0400

    FronEnd/Dev_002 - dropdown menu; first connection to lambda function_03

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('dropdownButton').addEventListener('click', function ()
     var dropdownContent = document.getElementById('dropdownContent');
     dropdownContent.innerHTML = '';
 
-    const url = new URL('https://y7hqsirgg2.execute-api.us-east-2.amazonaws.com/dev'); // Replace with your API endpoint
+    const url = new URL('https://y7hqsirgg2.execute-api.us-east-2.amazonaws.com/dev/'); // Replace with your API endpoint
     const params = {'agency': 'DDC', 'version': '01'};
         
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
